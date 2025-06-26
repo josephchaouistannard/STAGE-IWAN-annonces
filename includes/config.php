a967dfb7f06f35515e45030c85ec48065391a351
josephchaouistannard <josephchaouistannard@gmail.com>
2025-06-26 09:23:55 +0200

    changes logos to svg

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // CONTENU A CUSTOMISER
-$header_logo = "<img id=\"header-img\" src=\"assets/header.svg\">";
+$header_logo = "<img id=\"header-img\" src=\"assets/logo.svg\">";
 $header_text = "<h1>Maison de l'emploi Île de Noirmoutier</h1>";
 
 $footer_text_gauche = "<p><strong>Communauté de Communes</strong></p>
@@ -12,6 +12,6 @@ $footer_text_gauche = "<p><strong>Communauté de Communes</strong></p>
                     <p><strong>Horaires d’ouverture</strong></p>
                     <p>Le lundi, mardi et jeudi de 9h00 à 12h30 et de 14h00 à 17H30<br>Le mercredi et vendredi de 9h00 à
                         12h30 (fermé l’après-midi)</p>";
-$footer_img_droite = "<img id=\"footer-img\" src=\"assets/footer.jpg\">";
+$footer_img_droite = "<img id=\"footer-img\" src=\"assets/logo.svg\">";
 
-// communes to add here, an array with correct names, and value from db. Need to change filter function, display checkboxes...
+// communes to add here, an array with correct names, and value from db. Need to change filter function, display checkboxes
