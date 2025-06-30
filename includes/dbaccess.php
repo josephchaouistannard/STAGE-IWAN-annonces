e220411df4d97a59b42521b31dbb1ca846768394
josephchaouistannard <josephchaouistannard@gmail.com>
2025-06-30 09:18:06 +0200

    mis a jour dbaccess pour gérer .json et .JSON

--- includes/dbaccess.php
+++ includes/dbaccess.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Class d'access aux données (actuellement pour fichier JSON)
- * 
+ *
  * Contient une méthode pour obtenir toutes les offres, non triées et sans préparation de présentation html etc
  * Et des methodes pour lire et enregistrer le fichier json du compteur de vues
  */
@@ -13,8 +13,18 @@ class Dbaccess
      */
     function chargerToutesOffresJSON()
     {
-        $file_contents = file_get_contents(dirname(__DIR__) . "/db.json");
-        $data = json_decode($file_contents, true);
+        $chemin_min = dirname(__DIR__) . "/db.json";
+        $chemin_maj = dirname(__DIR__) . "/db.JSON";
+
+        $contenu_fichier = file_get_contents($chemin_min);
+
+        // Si .json ne marche pas, essaie .JSON
+        if ($contenu_fichier === false || $contenu_fichier === '') {
+            $contenu_fichier = file_get_contents($chemin_maj);
+        }
+
+        $data = json_decode($contenu_fichier, true);
+
         if ($data) {
             return $data;
         }
@@ -25,8 +35,18 @@ class Dbaccess
      * Charge le vues par offre dans compteur.json, retourne un array associatif
      */
     function chargerVues() {
-        $file_contents = file_get_contents(dirname(__DIR__) . "/compteur.json");
-        $data = json_decode($file_contents, true);
+        $chemin_min = dirname(__DIR__) . "/compteur.json";
+        $chemin_maj = dirname(__DIR__) . "/compteur.JSON";
+
+        $contenu_fichier = file_get_contents($chemin_min);
+
+        // Si .json ne marche pas, essaie .JSON
+        if ($contenu_fichier === false || $contenu_fichier === '') {
+             $contenu_fichier = file_get_contents($chemin_maj);
+        }
+
+        $data = json_decode($contenu_fichier, true);
+
         if ($data) {
             return $data;
         }
@@ -40,6 +60,7 @@ class Dbaccess
      */
     function enregistrerVues($data_compteur_offres) {
         $json_data = json_encode($data_compteur_offres, JSON_PRETTY_PRINT);
+        // Toujours enregistrer compteur.json
         file_put_contents(dirname(__DIR__) . "/compteur.json", $json_data);
     }
 }
