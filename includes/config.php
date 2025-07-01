4d5924cceb5df96ba154af6dfb8487b0fc5570dc
josephchaouistannard <josephchaouistannard@gmail.com>
2025-07-01 09:47:27 +0200

    changement texte pied de page

--- includes/config.php
+++ includes/config.php
@@ -14,11 +14,15 @@ $cacheOffres = __DIR__ . '/../cache/toutesOffres.cache';
 $header_logo = "<img id=\"header-img\" src=\"assets/logo.svg\" width=\"120\" height=\"60\">";
 $header_text = "<h1>Maison de l'emploi Île de Noirmoutier</h1>";
 
-$footer_text_gauche = "<p><strong>Communauté de Communes</strong></p>
-                    <p>51 rue de la Prée au Duc<br>85330 Noirmoutier en l’île<br>02 51 35 89 89</p>
+$footer_text_gauche = "<p><strong>Maison de l'Emploi - France services de l'île de Noirmoutier</strong></p>
+                    <p>(Service de la Communauté de Communes)<br>
+                    11 rue de la Prée-au-Duc<br>
+                    85330 Noirmoutier en l’île<br>
+                    02 51 39 32 18<br>
+                    emploi@iledenoirmoutier.org<br>
                     <p><strong>Horaires d’ouverture</strong></p>
-                    <p>Le lundi, mardi et jeudi de 9h00 à 12h30 et de 14h00 à 17H30<br>Le mercredi et vendredi de 9h00 à
-                        12h30 (fermé l’après-midi)</p>";
+                    <p>Lundi, mardi, jeudi : 8h30 à 12h / 13h15 à 17h<br>
+                    Mercredi et vendredi : 8h30 à 12h</p>";
 $footer_img_droite = "<img id=\"footer-img\" src=\"assets/logo.svg\">";
 
 // communes to add here, an array with correct names, and value from db. Need to change filter function, display checkboxes
